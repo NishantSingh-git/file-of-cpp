85a0b8424287a07fff189ca1ec0f9b992334c20e
NishantSingh-git <nishantsinghnishant3124@gmail.com>
2026-08-06 15:13:20 +0530

    c++ basic

---
+++ 5.recursion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+int add(int a ,int b){
+    return (a+b);
+}
+int main(){
+int a,b;
+cout<<"enter two numbers which you want to add:";
+cin >>a>>b;
+
+
+cout <<"sum of the  given two numbers:"<<add(a,b);
+
+}
+
