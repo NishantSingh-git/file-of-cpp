05932aa4196fc3ec8461e1b43d1674aa5729fbc7
NishantSingh-git <nishantsinghnishant3124@gmail.com>
2026-08-06 15:39:13 +0530

    c++ recursion+function

--- 5.recursion.cpp
+++ 5.recursion.cpp
@@ -3,13 +3,25 @@ using namespace std ;
 int add(int a ,int b){
     return (a+b);
 }
+void f(int x){
+    if(x>0){
+        cout<<x;
+        f(x-2);
+        cout<<x;
+    }
+}
 int main(){
-int a,b;
+int a,b,c;
 cout<<"enter two numbers which you want to add:";
 cin >>a>>b;
 
 
 cout <<"sum of the  given two numbers:"<<add(a,b);
+// recursion 
+cout<<" enter the number for recursion:";
+cin>>c;
+f(c);
+return 0;
 
 }
 
